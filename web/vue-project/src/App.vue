558b641b30f856eafc3ee214edb3dc9bc9019761
EGVPedralbes <a20erigomvil@inspedralbes.cat>
2024-02-27 09:56:44 +0100

    AddVue

---
+++ web/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div id="app">
+    <table>
+      <tr>
+        <td @click="console.log(1)">1</td>
+        <td @click="console.log(2)">2</td>
+        <td @click="console.log(3)">3</td>
+      </tr>
+      <tr>
+        <td @click="console.log(4)">4</td>
+        <td @click="console.log(5)">5</td>
+        <td @click="console.log(6)">6</td>
+      </tr>
+      <tr>
+        <td @click="console.log(7)">7</td>
+        <td @click="console.log(8)">8</td>
+        <td @click="console.log(9)">9</td>
+      </tr>
+    </table>
+  </div>
+</template>
